95a369852c4910e34a4eae8e5b3307c0e41bfcc0
Alexey08 <bauer080@gmail.com>
2022-10-14 17:17:29 +0500

    Initial Commit

---
+++ PatternObserver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatternObserver
+//
+//  Created by Алексей Уланов on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
